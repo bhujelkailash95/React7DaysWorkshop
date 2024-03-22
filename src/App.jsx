0d387c7afccf9d8b4e0f772073f5e2d6d892c2db
Kailash Bhujel <bhujelkailash88@gmail.com>
2024-03-22 12:02:36 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import About from './assets/About'
+import Home from './assets/Home'
+import Contact from './assets/Contact'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
